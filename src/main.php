a20fe3e8d03af9010765ca05e859373715c6a7cf
plugmcpe <M7mmdgamer7@icloud.com>
2016-08-24 05:26:47 +0300

    Create main.php

---
+++ src/main.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PluginA5t9ar;
+
+use pocketmine/plugin/PluginBase;
+use pocketmine/command/Command;
+use pocketmine/text/text Format as Color;
+use pocketmine/Command/CommandSender
+
